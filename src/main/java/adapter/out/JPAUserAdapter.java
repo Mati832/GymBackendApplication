900e3dbfa62e84729421acb4d197126207886f24
Satkar Singh <sathanda65@gmail.com>
2025-12-05 12:42:46 +0100

    implemented JPAUserMapper

--- src/main/java/adapter/out/JPAUserAdapter.java
+++ src/main/java/adapter/out/JPAUserAdapter.java
@@ -1,24 +1,13 @@
 package adapter.out;
-
-import adapter.mapper.UserMapper;
-
-import adapter.out.Entities.CoachEntity;
-import adapter.out.Entities.MemberEntity;
-
-import static adapter.mapper.UserMapper.toDomain;
+import static adapter.mapper.JPAUserMapper.*;
 import adapter.out.Entities.UserEntity;
 import application.port.out.FindUserByEmailPort;
 import application.port.out.FindUserByIdPort;
 import application.port.out.SaveUserPort;
-import domain.model.Coach;
-import domain.model.Member;
 import domain.model.User;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 
 @ApplicationScoped
@@ -47,28 +36,5 @@ public class JPAUserAdapter implements SaveUserPort, FindUserByEmailPort, FindUs
     @Override
     public User findUserById(Long id) {
         return toDomain(em.find(UserEntity.class, id));
-    }
-
-
-    //For Mapping from Domain <-> Persistence
-
-    //Every Adapter looks up and finds the Entity by id. This is done by the adapter and not the Mapper
-    private UserEntity toEntity(User user) {
-        if(user instanceof Member member){
-           MemberEntity memberEntity = UserMapper.toEntity(member);
-           memberEntity.setCoaches(member.getCoaches().stream().map(id -> em.find(CoachEntity.class, id)).toList());
-           return memberEntity;
-        }
-        Coach coach = (Coach)user;
-        CoachEntity coachEntity = UserMapper.toEntity(coach);
-        coachEntity.setClients(coach.getClients().stream().map(id -> em.find(MemberEntity.class, id)).toList());
-        return coachEntity;
-    }
-
-    private User toDomain(UserEntity entity) {
-        if(entity instanceof MemberEntity memberEntity){
-            return UserMapper.toDomain(memberEntity);
-        }
-        return UserMapper.toDomain((CoachEntity)entity);
     }
 }
